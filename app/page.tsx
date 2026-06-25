1a9d01affd2a4d0bcfeeebf52a18695942688415
gangsardamai <gangsardamai17@gmail.com>
2026-06-25 13:42:36 +0700

    Initial Dokter Ambis V2

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Dokter Ambis V2</h1>
+      <p>Deploy Test Berhasil 🚀</p>
+    </main>
+  );
+}
