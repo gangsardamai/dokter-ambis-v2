a1eae8cbc443f358ca321a661a3d68139b19e7d9
gangsardamai <gangsardamai17@gmail.com>
2026-07-23 13:38:56 +0700

    fix: pass create quiz server action directly

--- app/dashboard/quiz/new/page.tsx
+++ app/dashboard/quiz/new/page.tsx
@@ -51,13 +51,7 @@ export default async function NewQuizPage({
               })
             )}
             submitLabel="Simpan"
-            onSubmit={async (data) => {
-
-              await createQuizAction(
-                data
-              );
-
-            }}
+            onSubmit={createQuizAction}
           />
 
         </div>
@@ -67,5 +61,4 @@ export default async function NewQuizPage({
     </Container>
 
   );
-
 }
