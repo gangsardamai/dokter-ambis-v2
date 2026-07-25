e2591103d958b2cd2396aa62e6fcbc76b74c73e9
gangsardamai <gangsardamai17@gmail.com>
2026-07-25 11:23:55 +0700

    feat: combine application and tryout Supabase types

---
+++ supabase/types/database.extended.types.ts
@@ -0,0 +1,9 @@
+import type { Database as ApplicationDatabase } from "./database.app.types";
+import type { TryoutFunctions, TryoutTables } from "./tryout.types";
+
+export type Database = Omit<ApplicationDatabase, "public"> & {
+  public: Omit<ApplicationDatabase["public"], "Tables" | "Functions"> & {
+    Tables: ApplicationDatabase["public"]["Tables"] & TryoutTables;
+    Functions: ApplicationDatabase["public"]["Functions"] & TryoutFunctions;
+  };
+};
