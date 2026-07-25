aae5b6922dce36624a74cf45a1fc261a7ae1e38c
gangsardamai <gangsardamai17@gmail.com>
2026-07-25 11:57:29 +0700

    feat: include tryout followup RPC types

--- supabase/types/database.extended.types.ts
+++ supabase/types/database.extended.types.ts
@@ -1,9 +1,12 @@
 import type { Database as ApplicationDatabase } from "./database.app.types";
+import type { TryoutFollowupFunctions } from "./tryout.followup.types";
 import type { TryoutFunctions, TryoutTables } from "./tryout.types";
 
 export type Database = Omit<ApplicationDatabase, "public"> & {
   public: Omit<ApplicationDatabase["public"], "Tables" | "Functions"> & {
     Tables: ApplicationDatabase["public"]["Tables"] & TryoutTables;
-    Functions: ApplicationDatabase["public"]["Functions"] & TryoutFunctions;
+    Functions: ApplicationDatabase["public"]["Functions"] &
+      TryoutFunctions &
+      TryoutFollowupFunctions;
   };
 };
